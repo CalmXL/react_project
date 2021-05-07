e173209dc4fa76bcefd678e82faafd4881887e9d
xulei <xl18845898715@gmail.com>
2021-05-07 21:10:22 +0800

    the first update

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React,{Component} from 'react'
+import {Switch,Route} from 'react-router-dom'
+import Login from './pages/login/login'
+import Admin from './pages/admin/admin'
+
+export default class App extends Component{
+    render(){
+        return(
+          <div className='app'>
+            <Switch>
+              <Route path='/login' component={Login}/>
+              <Route path='/admin' component={Admin}/>
+            </Switch>
+          </div>
+        )
+    }
+}
